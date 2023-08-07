d7e5de2b7973c140e419cba979896608032bb3ce
Shamil65 <shamilziganshin132@gmail.com>
2023-08-07 22:44:39 +0500

    Подключение лайков и дизлайков

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -26,7 +26,7 @@ class AddComments(View):
             form.save()
         return redirect(f'/{pk}')
 
-class get_client_ip(request):
+def get_client_ip(request):
     x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
     if x_forwarded_for:
         ip = x_forwarded_for.split(',')[0]
